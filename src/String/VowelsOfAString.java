ecba23356e610476baa458f21796f358a2aaa422
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-18 22:34:51 +0530

    Update VowelsOfAString.java

--- src/String/VowelsOfAString.java
+++ src/String/VowelsOfAString.java
@@ -4,7 +4,7 @@ public class VowelsOfAString {
     public static void main(String[] args) {
         System.out.print("Enter a string : ");
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str = sc.nextLine();// nextLine() method reads a string that includes spaces
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u'){
                 System.out.print(str.charAt(i)+" "+i);// charAt() method returns the character at the specified index in a string.
