24822c4396c3d72f57cd82de2e45cc525f5f0c5c
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-18 23:40:42 +0530

    Create SwapTwoNumbers.java
    Write A Method to swap two numbers using a method.

---
+++ src/Methods/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Swap(10,20);
+    }
+    public static void Swap(int a,int b){
+        System.out.println("Before Swapping "+"a = "+a +" "+"b = "+b);
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping "+"a = "+a+" b = "+b);
+    }
+}
