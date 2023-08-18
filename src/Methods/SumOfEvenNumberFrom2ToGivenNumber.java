cea49909889cd89334653bccf2256efbcb4346c7
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-18 23:44:06 +0530

    Create SumOfEvenNumberFrom2ToGivenNumber.java
    Write A Method to sum of even number from 2 to given number using method and Random class

---
+++ src/Methods/SumOfEvenNumberFrom2ToGivenNumber.java
@@ -0,0 +1,17 @@
+package Methods;
+import java.util.*;
+public class SumOfEvenNumberFrom2ToGivenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        Sum(num);// calling Sum() method
+    }
+    public static void Sum(int num){// Sum() method
+        int sum=0;
+        for(int i=2;i<=num;i=i+2){
+            sum=sum+i;// sum of even numbers from 2 to num
+        }
+        System.out.println("The sum of even numbers from 2 to "+num+" is "+sum);
+    }
+}
