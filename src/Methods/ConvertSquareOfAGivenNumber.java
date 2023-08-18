5571317a44150210c5acb5b95e7a7a8f69a3531f
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-18 23:42:36 +0530

    Create ConvertSquareOfAGivenNumber.java
    Write A Method to convert square of a given number using method

---
+++ src/Methods/ConvertSquareOfAGivenNumber.java
@@ -0,0 +1,14 @@
+package Methods;
+import java.util.*;
+public class ConvertSquareOfAGivenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        Square(num);// calling Square() method
+    }
+    public static void Square(int num){// Square() method
+        int square=num*num;// square of a number is the number multiplied by itself
+        System.out.println("The square of "+num+" is "+square);
+    }
+}
