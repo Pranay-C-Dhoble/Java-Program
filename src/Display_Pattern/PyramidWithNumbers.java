96208f0edc386a7f5a8e4325f98aa61c10200490
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-19 17:42:08 +0530

    Create PyramidWithNumbers.java
    Write a Program to make such a pattern like a pyramid with numbers increased by
        1
       2 3
      4 5 6
     7 8 9 10

---
+++ src/Display_Pattern/PyramidWithNumbers.java
@@ -0,0 +1,17 @@
+package Display_Pattern;
+
+public class PyramidWithNumbers {
+    public static void main(String[] args) {
+        int temp = 1;
+        for(int i = 1; i <= 4; i++){
+            for(int j = 4; j>= i; j--){// Start from 4 and decrement
+                    System.out.print(" ");//space
+            }
+               for(int x = 1; x <= i; x++){
+                   System.out.print(temp+" ");
+                   temp++;//for incrementing
+               }
+            System.out.println();//new line
+        }
+    }
+}
