31c2be31f7e88eb21f91bf0ffd6b23bc0a71c28a
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-20 01:22:44 +0530

    Create SearchInsertPosition.java
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.

---
+++ src/Methods/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package Methods;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,5,6};
+        int target = 3;
+        int ans = searchInsert(arr,target);
+        System.out.println("length of actual array : "+ans);
+    }
+
+    public static int searchInsert(int []arr,int target){
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]==target){
+                return j;
+            }
+            else if(arr[j]>target){
+                return j;
+            }
+        }
+        return arr.length;
+    }
+}
