7733e51f14a3abf7afec103d071872b5b569224a
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-20 13:57:36 +0530

    Create Array_divisible_by_3_5.java

---
+++ src/SimpleArrayPrograms/Array_divisible_by_3_5.java
@@ -0,0 +1,11 @@
+package SimpleArrayPrograms;
+public class Array_divisible_by_3_5 {
+    public static void main(String[] args) {
+        int []a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 30, 35, 20};
+        for (int j : a) {
+            if (j % 3 == 0 && j % 5 == 0) {
+                System.out.println("Divisible by 3 and 5 is :" + j);
+            }
+        }
+    }
+}
