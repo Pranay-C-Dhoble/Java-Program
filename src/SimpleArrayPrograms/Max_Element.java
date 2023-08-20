768e00514226b37cc534472ac81991b661806acb
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-20 13:58:49 +0530

    Create Max_Element.java

---
+++ src/SimpleArrayPrograms/Max_Element.java
@@ -0,0 +1,13 @@
+package SimpleArrayPrograms;
+public class Max_Element {
+    public static void main(String[] args) {
+        int []a = {98,20,10,70,40,101,70,80,45,26,100};
+        int temp = a[0];
+        for(int i = 1; i < a.length; i++){
+            if (temp < a[i]){
+                temp = a[i];
+            }
+        }
+        System.out.println("Maximum Element of this Array is : "+temp);
+    }
+}
