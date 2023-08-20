63c18acac5dac2636567bbbdd662477988648080
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-20 13:25:28 +0530

    Update RunningSumOfAnArray.java

--- src/ArrayProgramsUsingMethods/RunningSumOfAnArray.java
+++ src/ArrayProgramsUsingMethods/RunningSumOfAnArray.java
@@ -1,4 +1,4 @@
-package Methods;
+package ArrayProgramsUsingMethods;
 public class RunningSumOfAnArray {
     public static void main(String[] args) {
         int [] arr ={1,2,3,4,5,6,7,8,9,10};// declaring and initializing array
