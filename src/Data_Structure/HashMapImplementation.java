3cb2a52e7d5b8f7d5d5c7d0af2c5e9b214264721
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-23 15:00:01 +0530

    Create HashMapImplementation.java

---
+++ src/Data_Structure/HashMapImplementation.java
@@ -0,0 +1,34 @@
+package Data_Structure;
+
+
+
+
+
+//Hashmap Functions
+//1. put() --> Inserting Elements
+//2. size() --> Size of the Map
+//3. containsKey() --> Searching Elements
+//4. containsValue() --> Searching Elements
+//5. get() --> Searching Elements
+//6. remove() --> Deleting Elements
+//7. clear() --> Deleting Elements
+//8. keySet() --> Iterating Elements
+//9. values() --> Iterating Elements
+//10. entrySet() --> Iterating Elements
+
+
+//****** HashMap internally implemented as linked list and hash table
+//****** Hash table is a array of linked list
+//****** Hash table is a array of buckets
+
+//Hashing ---> Changing data into some other form
+//eg : 123 --> 321
+//eg : 123 --> 321 --> 3*10^0 + 2*10^1 + 1*10^2
+//eg : password --> abc ---> 123
+
+
+public class HashMapImplementation {
+    public static void main(String[] args) {
+
+    }
+}
