a057ae78a674fe00d36ba305ed8cdd743775481d
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 00:15:51 +0530

    Update Take_Input.java

--- src/SimpleArrayPrograms/Take_Input.java
+++ src/SimpleArrayPrograms/Take_Input.java
@@ -2,17 +2,22 @@ package SimpleArrayPrograms;
 import java.util.*;
 public class Take_Input {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length of array : ");
-        int n=sc.nextInt();
-        System.out.print("Enter Elements in Array : ");
-        int [] array = new int[n];
-           for (int i= 0;i<n;i++) {
-               array[i] = sc.nextInt();
-           }
+        int [] array = takeInput();
            System.out.print("the array output are : ");
            for(int j : array){
                System.out.print(j +" ");
            }
+    }
+
+    public static int[] takeInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array : ");
+        int n = sc.nextInt();
+        System.out.print("Enter Elements in Array : ");
+        int [] array = new int[n]; //array is a object of array class
+           for (int i= 0;i<n;i++) {
+               array[i] = sc.nextInt();
+           }
+        return array;
     }
 }
