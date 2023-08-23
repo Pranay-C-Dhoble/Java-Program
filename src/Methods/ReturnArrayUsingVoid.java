e8691d661baf8ed4c9736f655e557d4d338c6a5d
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 00:13:51 +0530

    Update ReturnArrayUsingVoid.java

--- src/Methods/ReturnArrayUsingVoid.java
+++ src/Methods/ReturnArrayUsingVoid.java
@@ -1,15 +1,19 @@
 package Methods;
-
+//********* java is always Call By Value *********
 public class ReturnArrayUsingVoid {
     public static void main(String[] args) {
         int []arr ={1,2,3,4,5};
-        reverseArray(arr);
+        int x = 5;
+        reverseArray(arr, x);
+        System.out.println(x);
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
-    static void reverseArray(int []arr){
-        arr[0] = 5;
+    static void reverseArray(int [] arr, int i){    //why array is change but i is not change?
+                                                    //because array is reference type and i is primitive type
+        arr [0] = 7;                                //Reference means address
+        i = 8;                                      //Primitive means value
     }
 
 }
