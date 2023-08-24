9b917f696c05e23e1cdfb8c10fc70dc53a7187c6
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 15:16:09 +0530

    Create fibonacciSeries.java

---
+++ src/Methods/fibonacciSeries.java
@@ -0,0 +1,21 @@
+package Methods;
+
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        int result = fibonacci(6);
+        System.out.println(result);
+    }
+    static int fibonacci(int n){
+        if(n == 1 || n == 2){
+            return n - 1;
+        }
+        else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        // fibonacci series 0 1 1 2 3 5
+        //5-1 = 4 + 5-2 = 3
+        //4-1 = 3 + 4-2 = 2
+        //3-1 = 2 + 3-2 = 1
+        //2-1 = 1 + 2-2 = 0
+    }
+}
