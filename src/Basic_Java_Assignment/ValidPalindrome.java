3d9fcec3e2ec150b5552b41a62118d38bb441994
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 19:36:11 +0530

    Create ValidPalindrome.java
    Input: s = "A man, a plan, a canal: Panama"
    Output: true
    Explanation: "amanaplanacanalpanama" is a palindrome.

---
+++ src/Basic_Java_Assignment/ValidPalindrome.java
@@ -0,0 +1,27 @@
+package Basic_Java_Assignment;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean result = isPalindrome(s);
+        System.out.println(result);
+    }
+    static boolean isPalindrome(String s){
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){ //isLetterOrDigit() is a method and return type is boolean
+                i++;                                                 //Character is a class
+            }
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){//toLowerCase() is a method and return type is char
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
