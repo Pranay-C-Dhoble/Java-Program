3afb0130152c7a300e2625e8f4393fe19118bbe1
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 15:56:29 +0530

    Create MaximumNumberOfWordsFoundInSentences.java
    Return the maximum number of words that appear in a single sentence.

---
+++ src/Basic_Java_Assignment/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,24 @@
+package Basic_Java_Assignment;
+
+public class MaximumNumberOfWordsFoundInSentences {
+    public static void main(String[] args) {
+        String [] sent ={"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        int result = maxNumberOfWords(sent);
+        System.out.println(result);
+    }
+    static int maxNumberOfWords(String [] sent){
+        int max = 0;
+        for(String s : sent){
+            int count = 0;
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == ' '){
+                    count++;
+                }
+            }
+            if(count > max){
+                max = count;
+            }
+        }
+        return max + 1; //because we have to count the last word also
+    }
+}
