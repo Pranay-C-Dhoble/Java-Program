1dec9d2e0511bcbeb8307feb3c204dee12073942
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-24 19:40:09 +0530

    Create OopsInformation.java
    Information About Oops concept

---
+++ src/OopsConcept/OopsInformation.java
@@ -0,0 +1,23 @@
+package OopsConcept;
+
+//Encapsulation is a process of wrapping code and data together into a single unit.
+//It is a way to achieve data hiding in Java
+//because other class will not be able to access the data through the private data members.
+
+//Inheritance is a mechanism in which one class acquires the property of another class.
+//For example, a child inherits the traits of his/her parents.
+//types of inheritance: single, multilevel, hierarchical, multiple, hybrid
+//java not support multiple inheritance. but we can achieve it by using interface.
+
+//Polymorphism is the ability of an object to take on many forms.
+//The most common use of polymorphism in OOP occurs when a parent class reference is used to refer to a child class object.
+//Any Java object that can pass more than one IS-A test is considered to be polymorphic.
+
+//Abstraction is a process of hiding the implementation details and showing only functionality to the user.
+//Another way, it shows only essential things to the user and hides the internal details, for example, sending SMS where you type the text and send the message.
+//You don't know the internal processing about the message delivery.
+public class OopsInformation {
+    public static void main(String[] args) {
+
+    }
+}
