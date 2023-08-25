9099117c0d440219c08aad6c4b647fbc70fceb50
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-25 23:48:08 +0530

    Create DOBSplit.java
    Split String into Date, Month, and Year

---
+++ src/String/DOBSplit.java
@@ -0,0 +1,21 @@
+package String;
+import java.util.Scanner;
+public class DOBSplit {
+    public static void main(String[] args) {
+        String s = Dob();
+        System.out.println(s);
+    }
+    static String Dob(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your DOB in dd/mm/yyyy format: ");
+        String dob = sc.nextLine(); //nextLine() method is used to get the input as a string.
+        //dob is a string variable that stores the date of birth in dd/mm/yyyy format.
+        String [] arr = dob.split("/");
+        //split() method splits the string based on the given regular expression or delimiter, and returns the tokens in the form of array.
+        return "Date: " + arr[0] + "\nMonth: " + arr[1] + "\nYear: " + arr[2];
+    }
+}
+//difference between if else and switch case:
+//if else is used when we have to check multiple conditions.
+//switch case is used when we have to check only one variable.
+//which one is faster? switch case is faster than if else.
