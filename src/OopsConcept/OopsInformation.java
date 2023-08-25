ae977a58d0d1e0e63d991157ce8e201daaf40a31
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-25 23:48:21 +0530

    Update OopsInformation.java

--- src/OopsConcept/OopsInformation.java
+++ src/OopsConcept/OopsInformation.java
@@ -3,6 +3,7 @@ package OopsConcept;
 //Encapsulation is a process of wrapping code and data together into a single unit.
 //It is a way to achieve data hiding in Java
 //because other class will not be able to access the data through the private data members.
+//types of modifiers: public, private, protected, default
 
 //Inheritance is a mechanism in which one class acquires the property of another class.
 //For example, a child inherits the traits of his/her parents.
@@ -13,6 +14,23 @@ package OopsConcept;
 //in other words, one object has many forms or has one name with multiple functionalities.
 //The most common use of polymorphism in OOP occurs when a parent class reference is used to refer to a child class object.
 //Any Java object that can pass more than one IS-A test is considered to be polymorphic.
+
+//Static Binding and Dynamic Binding
+//Compile time polymorphism: *** method overloading
+//Runtime polymorphism: *** method overriding
+
+//method overriding rules:
+//1. method name must be same.
+//2. method arguments must be same.
+//3. method return type must be same.
+//4. method access modifier must be same or higher.
+//5. method must not be static.
+//6. method must not be final.
+//7. method must not be private.
+//8. method must not be constructor.
+
+
+
 
 //Abstraction is a process of hiding the implementation details and showing only functionality to the user.
 //Another way, it shows only essential things to the user and hides the internal details, for example, sending SMS where you type the text and send the message.
