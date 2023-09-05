22b1a33c76bc3e05339879a4be5cd237839fbd20
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-05 19:05:01 +0530

    Update CreateOurOwnJavaClass.java

--- src/OopsConcept/CreateOurOwnJavaClass.java
+++ src/OopsConcept/CreateOurOwnJavaClass.java
@@ -4,6 +4,13 @@ public class CreateOurOwnJavaClass {
     public static void main(String[] args) {
         System.out.println("This is my first java class.");
         MyFirstJavaClass obj = new MyFirstJavaClass();
+
+        //MyFirstJavaClass is a Constructor.
+        //why ? because it has the same name as its class name.
+        //Constructor is a block of code that initializes the newly created object.
+
+        //new is a keyword that is used to create an object.
+        //Dynamically allocating memory for an object at runtime.
         //here obj is an object of MyFirstJavaClass class.
 
         //why we need to create object?
@@ -25,7 +32,7 @@ public class CreateOurOwnJavaClass {
 class MyFirstJavaClass { //public class MyFirstJavaClass is not allowed.//Why ?//Because we can have only one public class in a java file.
 
     int id;// ----->Attributes or data members or instance variables
-    String name;
+    String name; //-----> instance variables is im
 
     public static void Class(String[] args) {
         int a = 10;// ----->Local variable
