252c3314d4829b30b7186a0c258c7d72b8805b3b
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-08 19:26:27 +0530

    Update BoxArea.java

--- src/Basic_Java_Assignment/BoxArea.java
+++ src/Basic_Java_Assignment/BoxArea.java
@@ -1,18 +1,24 @@
 package Basic_Java_Assignment;
 import java.util.*;
 public class BoxArea {
+    int length;
+    int breadth;
+    int height;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length of the box: ");
-        int l = sc.nextInt();
+        int length = sc.nextInt();
         System.out.print("Enter the breadth of the box: ");
-        int b = sc.nextInt();
+        int breadth = sc.nextInt();
         System.out.print("Enter the height of the box: ");
-        int h = sc.nextInt();
-        int result = area(l, b, h);
+        int height = sc.nextInt();
+        
+        BoxArea obj = new BoxArea();
+        int result = obj.area(length, breadth, height);
         System.out.println("Area of the box is: " + result);
     }
-    public static int area(int l, int b, int h){
+    private int area(int l, int b, int h){
         int result = l * b * h;
         return result;
     }
