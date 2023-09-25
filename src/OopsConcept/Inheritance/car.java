475e07c79f1916e75f4a6bc7f239ce5bd450713b
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-25 18:27:01 +0530

    Create car.java

---
+++ src/OopsConcept/Inheritance/car.java
@@ -0,0 +1,11 @@
+package OopsConcept.Inheritance;
+
+public class car extends vehicle{
+
+    void test(){
+        color = "khk";
+        speed = 3000;
+        System.out.println(color + " " + speed);
+    }
+
+}
