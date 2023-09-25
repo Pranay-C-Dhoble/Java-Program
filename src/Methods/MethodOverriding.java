39a357e829f76f679d32d96cbd71836db8f23624
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-25 19:20:46 +0530

    Create MethodOverriding.java

---
+++ src/Methods/MethodOverriding.java
@@ -0,0 +1,19 @@
+package Methods;
+
+// Method Overriding is a feature that allows a subclass or child class to provide a specific implementation of a method that is already provided by one of its super-classes or parent classes.
+// When a method in a subclass has the same name, same parameters or signature and same return type(or sub-type) as a method in its super-class, then the method in the subclass is said to override the method in the super-class.
+// Method overriding is one of the ways in which Java supports Runtime Polymorphism.
+
+// Rules for method overriding:
+// 1. The method must have the same name as in the parent class
+// 2. The method must have the same parameter as in the parent class.
+// 3. There must be an IS-A relationship (inheritance).
+// 4. The return type must be the same or a subtype of the return type declared in the parent class method.
+
+// Why we use method overriding?
+// 1. To provide specific implementation of a method that is already provided by its super class.
+// 2. To provide the specific implementation of a method that is already provided by its parent class.
+// 3. Method overriding is used for runtime polymorphism.
+// 4. Method overriding is used to provide the specific implementation of a method which is already provided by its super class.
+public class MethodOverriding {
+}
