8aaddf2f558a165d9b3e88bd81362e58ac51c669
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-25 18:30:26 +0530

    Create finalKeywordInformation.java

---
+++ src/OopsConcept/finalKeywordInformation.java
@@ -0,0 +1,77 @@
+package OopsConcept;
+
+public class finalKeywordInformation {
+    //inner class?
+    //A class which is created inside another class is known as inner class.
+
+
+
+    //overiding?
+    //Can we override static method in java?
+    //No, we cannot override static method in java.
+    //Why ? because static method is bound with class whereas instance method is bound with an object.
+
+    //method hiding?
+    //Can we override static method in java?
+
+    //Can we inherit static method in java?
+    //Yes, we can inherit static method in java.
+    //Why ? because static method is bound with class whereas instance method is bound with an object.
+
+    //Can we override private method in java?
+    //No, we cannot override private method in java.
+
+    //Can we override final method in java?
+    //No, we cannot override final method in java.
+
+    //Can we override static final method in java?
+    //No, we cannot override static final method in java.
+
+    //what is singalton class?
+    //A class that can create only one object is known as a singleton class.
+
+    //what is the purpose of singalton class?
+    //The purpose of singleton class is to control object creation.
+
+    //why string is immutable in java?
+    //String is immutable in java because java uses the string pool.
+    //String pool is a fixed area in heap memory.
+
+    //scp?
+    //String Constant Pool (SCP) is a special memory location in Java heap.
+    //When we create a string using string literal, that string will be stored in SCP.
+
+    //== vs equals()?
+    //== is an operator whereas equals() is a method.
+    //== is used to compare the reference of two objects whereas equals() is used to compare the content of two objects.
+
+
+    //type casting?
+    //Type casting is when you assign a value of one primitive data type to another type.
+
+    //autoboxing?
+    //The automatic conversion of primitive data types into its corresponding wrapper class is known as autoboxing.
+
+    //jdk vs jre vs jvm?
+    //JDK stands for Java Development Kit.
+    //JDK is Platform Dependent Software.
+    //JRE stands for Java Runtime Environment.
+    //JRE is also Platform Dependent Software.
+    //JVM stands for Java Virtual Machine.  // interpreter
+    //JDK is a software development kit whereas JRE is a software bundle that allows Java program to run.
+
+    //jmm?
+    //JMM stands for Java Memory Model.
+    //JMM is a specification that describes the working of the Java Virtual Machine.
+
+    //marker interface?
+    //An interface that has no data member and method is known as a marker interface.
+    //For example: Serializable, Cloneable, Remote, etc.
+
+    //serializable interface?
+    //Serializable is a marker interface.
+    //It is used to "mark" java classes so that objects of these classes may get certain capability.
+
+    //It is used to provide special instructions to the JVM.
+
+}
