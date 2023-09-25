7794baa1a31ce796d4131dfa894782e901855b9c
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-09-25 18:27:22 +0530

    Create vehicle.java

---
+++ src/OopsConcept/Inheritance/vehicle.java
@@ -0,0 +1,22 @@
+package OopsConcept.Inheritance;
+
+public class vehicle {
+    String color;
+    int wheels;
+    int speed;
+    String name;
+    String model;
+
+    void start(){
+        System.out.println("Vehicle is starting");
+    }
+    void stop(){
+        System.out.println("Vehicle is stopping");
+    }
+    void refuel(){
+        System.out.println("Vehicle is refueling");
+    }
+    void theftSafety(){
+        System.out.println("Vehicle has theft safety");
+    }
+}
