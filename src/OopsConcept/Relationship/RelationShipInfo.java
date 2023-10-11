861c0db4b1dce917e68252b80a3be74ff0a3ee84
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-11 19:04:23 +0530

    Create RelationShipInfo.java

---
+++ src/OopsConcept/Relationship/RelationShipInfo.java
@@ -0,0 +1,27 @@
+package OopsConcept.Relationship;
+//Types of Relationship in Java
+//There are 3 types of relationship in java:
+//1) IS-A Relationship.
+//2) HAS-A Relationship.
+//3) USES-A Relationship.
+
+// IS-A Relationship : In Java, a class can inherit another class. This is called IS-A relationship.
+//1) Inheritance : In Java, a class can inherit another class. This is called inheritance.
+
+
+// HAS-A Relationship : In Java, a class can contain another class. This is called HAS-A relationship.
+// 1) Aggregation (Weak Association)
+// 2) Composition (Strong Association)
+
+
+// Association (Association is relation between two separate classes which establishes through their Objects.)
+
+//1) Aggregation : In Java, a class can have an entity reference. This is called Aggregation.
+// Aggregation represents is Weak Association.
+//2) Composition : In Java, if a class contains an entity reference, it is known as composition.
+// Composition represents a Strong Association.
+
+// USES-A Relationship : In Java, a class can use another class. This is called USES-A relationship.
+
+public class RelationShipInfo {
+}
