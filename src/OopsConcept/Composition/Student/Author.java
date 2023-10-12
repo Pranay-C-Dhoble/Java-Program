24cdd3eb134ccc4434325f7de959b35cd8a4e3c9
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-12 19:32:55 +0530

    Create Author.java

---
+++ src/OopsConcept/Composition/Student/Author.java
@@ -0,0 +1,10 @@
+package OopsConcept.Composition.Student;
+
+public class Author {
+    //Properties of Author
+    //eg: name, address, email, phone
+    String name;
+    String address;
+    String email;
+    String phone;
+}
