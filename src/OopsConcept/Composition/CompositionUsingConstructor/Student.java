44bffde70a5ec368eb5315440dd68bdb2d71cf4d
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-13 19:28:24 +0530

    Create Student.java

---
+++ src/OopsConcept/Composition/CompositionUsingConstructor/Student.java
@@ -0,0 +1,22 @@
+package OopsConcept.Composition.CompositionUsingConstructor;
+
+public class Student {
+    //properties for Student
+     private String name;
+     private Book book;
+
+     //Composition using method
+    public void setBook(String name, Book book) {
+        this.name = name;
+        this.book = book;
+    }
+    //Composition using Constructor
+    Student(String name, Book book){
+        this.name = name;
+        this.book = book;
+    }
+    public void getBook() {
+        System.out.println(name + " Have a Book is " + book.name);
+    }
+
+}
