8e0b292cebe739bd38b32b3d0e24a3696d36e1ae
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-13 19:28:17 +0530

    Create Book.java

---
+++ src/OopsConcept/Composition/CompositionUsingConstructor/Book.java
@@ -0,0 +1,11 @@
+package OopsConcept.Composition.CompositionUsingConstructor;
+
+public class Book {
+    //properties for Book
+    String name;
+    String auther;
+    Book(String name, String auther){
+        this.name = name;
+        this.auther = auther;
+    }
+}
