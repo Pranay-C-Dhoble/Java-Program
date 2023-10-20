0cbbf80d0319270318b86d5de813aa20c65fca06
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:56:41 +0530

    Create DoorApp.java

---
+++ src/Composition/Door/DoorApp.java
@@ -0,0 +1,21 @@
+package Composition.Door;
+
+public class DoorApp {
+    public static void main(String[] args) {
+//        Door door = new Door();
+//        door.color = "Brown";
+//        door.size = "7ft";
+//        door.latch = new Latch();
+//        door.latch.color = "Silver";
+//        door.latch.size = "2in";
+//        door.lock = new Lock();
+//        door.lock.key = "1234";
+//        door.lock.type = "Electronic";
+//        System.out.println("Door color: " + door.color);
+//        System.out.println("Door size: " + door.size);
+//        System.out.println("Latch color: " + door.latch.color);
+//        System.out.println("Latch size: " + door.latch.size);
+//        System.out.println("Lock key: " + door.lock.key);
+//        System.out.println("Lock type: " + door.lock.type);
+    }
+}
