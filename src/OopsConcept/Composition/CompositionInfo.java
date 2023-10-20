288deb7c898f2adc1fff180a0a2be79a7e7bf0cf
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:51:58 +0530

    Update CompositionInfo.java

--- src/OopsConcept/Composition/CompositionInfo.java
+++ src/OopsConcept/Composition/CompositionInfo.java
@@ -1,4 +1,24 @@
 package OopsConcept.Composition;
+//What is Composition?
+//Composition is a restricted form of Aggregation in which two entities are highly dependent on each other.
+//It represents part-of relationship.
+//In composition, both the entities are dependent on each other.
+//When there is a composition between two entities, the composed object cannot exist without the other entity.
+//Let’s take an example of a relationship between House and Rooms.
+//House can contain multiple rooms - there is no independent life of room and any room cannot belong to two different houses.
+//If you delete the house - room will automatically be deleted.
+//Let’s take another example relationship between Questions and Options.
+//Single questions can have multiple options and option cannot belong to multiple questions.
+//If you delete questions - options will automatically be deleted.
+//Composition is a way to combine simple objects or data types into more complex ones.
+//Composition is used to represent part-of relationship.
+//Composition is also known as a “death” relationship.
+//In composition, both the entities are dependent on each other.
+//When there is a composition between two entities, the composed object cannot exist without the other entity.
 
+//composition having which type of relationship?
+//Composition having "has-a" relationship.
+//Composition is a strong Association.
+//Composition is a restricted form of Aggregation in which two entities are highly dependent on each other.
 public class CompositionInfo {
 }
