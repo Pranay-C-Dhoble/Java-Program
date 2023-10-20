c73c154ea140e650bb6baf76fb1dff3621fd4005
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:58:50 +0530

    Delete StudentMain.java

--- src/OopsConcept/Composition/CompositionUsingConstructor/StudentMain.java
+++
@@ -1,9 +0,0 @@
-package OopsConcept.Composition.CompositionUsingConstructor;
-
-public class StudentMain {
-    public static void main(String[] args) {
-        Book book = new Book("Java", "James Gosling");
-        Student student = new Student("Raj", book);
-        student.getBook();
-    }
-}
