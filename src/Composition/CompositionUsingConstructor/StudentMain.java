79548c424ddb4011460710187b2838f353b28bfd
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:56:33 +0530

    Create StudentMain.java

---
+++ src/Composition/CompositionUsingConstructor/StudentMain.java
@@ -0,0 +1,9 @@
+package Composition.CompositionUsingConstructor;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Book book = new Book("Java", "James Gosling");
+        Student student = new Student("Raj", book);
+        student.getBook();
+    }
+}
