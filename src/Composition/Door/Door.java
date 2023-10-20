c77b0c24b067b654c2be762192e1ead46a230e7d
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:56:36 +0530

    Create Door.java

---
+++ src/Composition/Door/Door.java
@@ -0,0 +1,22 @@
+package Composition.Door;
+
+public class Door {
+    //Dore Properties
+    private String color; //here why we are using private?
+    //because we don't want to expose the color property to the outside world.
+    //we want to control the access to the color property.
+    //we want to make sure that the color property is set only once.
+    //we want to make sure that the color property is set only to a valid value.
+
+
+    //How we can access the color property from outside the class?
+    //we can use a method called getColor() to access the color property.
+    //we can use a method called setColor() to set the color property.
+    private int height;
+    private int width;
+    private Lock lock;
+    private Latch latch;
+
+    //I want to make sure that the color property is set only once.
+    //how to do that?
+}
