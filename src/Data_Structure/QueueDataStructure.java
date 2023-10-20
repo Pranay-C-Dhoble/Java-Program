b60f8264e4d46b6931390737a1cffad30b08f603
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 12:04:07 +0530

    Create QueueDataStructure.java

---
+++ src/Data_Structure/QueueDataStructure.java
@@ -0,0 +1,18 @@
+package Data_Structure;
+
+//Queue is a Data Structure
+//Queue ---> First In First Out (FIFO)
+
+//Operations of Queue
+//1. Enqueue() --> Inserting Elements
+//2. Dequeue() --> Deleting Elements
+//3. isEmpty() --> Checking Queue is empty or not
+//4. isFull() --> Checking Queue is full or not
+//5. peek() --> Printing the first element of the Queue
+//6. deque() --> Double Ended Queue
+
+public class QueueDataStructure {
+    public static void main(String[] args) {
+
+    }
+}
