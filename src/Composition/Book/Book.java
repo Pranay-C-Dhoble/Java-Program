ccf0a816e1622fc370d0e9645c7efc0afff96c23
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:56:07 +0530

    Create Book.java

---
+++ src/Composition/Book/Book.java
@@ -0,0 +1,9 @@
+package Composition.Book;
+
+public class Book {
+    //Properties, DataMembers, Instance Variable
+    int NoPages;
+    String Title;
+    Publisher publisher;
+    Author author;
+}
