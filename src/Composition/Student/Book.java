671c3d1099788aeed4f4b5868018e9f1af5c0f8b
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-20 11:56:58 +0530

    Create Book.java

---
+++ src/Composition/Student/Book.java
@@ -0,0 +1,10 @@
+package Composition.Student;
+
+public class Book {
+    //Properties of Book
+    //eg: name, author, publisher, price
+    String bookname;
+    Author author;
+    Publisher publisher;
+    double price;
+}
