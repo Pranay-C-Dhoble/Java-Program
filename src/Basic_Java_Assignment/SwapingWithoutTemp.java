dad673ba348e85fdf8edc7af74bc9abed418b353
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-26 21:37:10 +0530

    Create SwapingWithoutTemp.java

---
+++ src/Basic_Java_Assignment/SwapingWithoutTemp.java
@@ -0,0 +1,13 @@
+package Basic_Java_Assignment;
+
+public class SwapingWithoutTemp {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swaping: a = " + a + " b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swaping: a = " + a + " b = " + b);
+    }
+}
