ab9d0e227f76c0f9342da9c6b92aae579c9898df
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-10-26 22:07:05 +0530

    Update ReverseString.java

--- src/String/ReverseString.java
+++ src/String/ReverseString.java
@@ -19,5 +19,12 @@ public class ReverseString {
          String reverse = "";
          reverse = new StringBuffer(str).reverse().toString();
          System.out.println("Reverse of " + str + " is " + reverse);
+         //what is the use of StringBuffer class?
+         //StringBuffer class is used to create mutable (modifiable) string.
+         //The StringBuffer class in java is same as String class except it is mutable i.e. it can be changed.
+            //Whereas String class once created cannot be changed.
+            //StringBuffer class is thread-safe i.e. multiple threads cannot access it simultaneously.
+            //StringBuffer class is synchronized i.e. it is thread-safe and hence can be used when we need to share StringBuffer between multiple threads.
+
      }
 }
