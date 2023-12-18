e76513be08f720a817f6e7b01fad8dfe9f1ee515
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-19 00:59:17 +0530

    Create User.java

---
+++ src/FirstNewJavaProject/User.java
@@ -0,0 +1,40 @@
+package FirstNewJavaProject;
+
+public class User {
+    private String Name;
+    private String Email;
+    private String Password;
+    private String Role;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String role) {
+        Role = role;
+    }
+}
