f08ac1e218faf4f5f7c2e1f1ad2b2f6c05eba9c0
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-19 00:59:03 +0530

    Create ExceptionHandelingInfo.java

---
+++ src/ExceptionHandeling/ExceptionHandelingInfo.java
@@ -0,0 +1,29 @@
+package ExceptionHandeling;
+//What is Exception?
+//Exception is an abnormal condition which occurs at runtime.
+//Exception is an event, which occurs during the execution of a program, that disrupts the normal flow of the program's instructions.
+//Exception is a problem that arises during the execution of a program.
+//Exception is an object which is thrown at runtime.
+//Exception is a problem that interrupts the normal flow of execution of a program.
+//Exception is a condition that causes a program to terminate abnormally.
+//Exception is a problem which may arise during the execution of a program.
+//Exception is a problem that arises unexpectedly and disrupts the normal flow of program.
+//Exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions.
+
+//What is Exception Handling?
+//Exception Handling is a mechanism to handle runtime errors such as ClassNotFoundException, IOException, SQLException, RemoteException, etc.
+//Exception Handling is a mechanism to handle the runtime errors such as ClassNotFoundException, IOException, SQLException, RemoteException, etc.
+
+//Why we use Exception?
+//Exception is used to handle the runtime errors.
+//Exception is used to handle the abnormal conditions that may occur during the execution of a program.
+//Exception is used to maintain the normal flow of the application.
+//Exception is used to specify the fault location and take appropriate action.
+//Exception is used to specify the alternative way to continue the normal flow of the program.
+//Exception is used to handle the runtime errors by using exception handling mechanism.
+//Exception is used to handle the abnormal conditions that may occur during the execution of a program by using exception handling mechanism.
+//Exception is used to maintain the normal flow of the application by using exception handling mechanism.
+//Exception is used to specify the fault location and take appropriate action by using exception handling mechanism.
+
+public class ExceptionHandelingInfo {
+}
