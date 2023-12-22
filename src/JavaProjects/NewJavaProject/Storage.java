29097e0f1bdf59ecd6c62a9274485ce9f30b9233
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-22 23:23:14 +0530

    Create Storage.java

---
+++ src/JavaProjects/NewJavaProject/Storage.java
@@ -0,0 +1,34 @@
+package JavaProjects.NewJavaProject;
+
+import JavaProjects.NewJavaProject.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    public static List<User> users = new ArrayList<>();
+
+    public static String getValue(String[] args, String key) {
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i].equals(key)) {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
+
+}
+
+//here getvalue is utility method
+//here we are using regular expression
+
+
+
+
+//static -- share among to all the objects
+
+//Singulton class --> only one object can be created
+
+// which class in java is singleton class --> Calendar class --> getInstance() --> return object of Calendar class
+
+//regular expression
