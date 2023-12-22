16f5aa06754fe27e61bf3afde140799ea60bb7e3
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-22 23:22:47 +0530

    Create CreateUser.java

---
+++ src/JavaProjects/NewJavaProject/Implement/CreateUser.java
@@ -0,0 +1,54 @@
+package JavaProjects.NewJavaProject.Implement;
+import JavaProjects.NewJavaProject.Storage;
+import JavaProjects.NewJavaProject.Validator.Command;
+import JavaProjects.NewJavaProject.Validator.CommandValidator;
+import JavaProjects.NewJavaProject.Model.User;
+
+public class CreateUser implements Command, CommandValidator {
+    public void execute(String[] args){
+        String name = Storage.getValue(args, "-n");
+        String phone = Storage.getValue(args, "-p");
+        String address = Storage.getValue(args, "-a");
+        String[] array = {name, phone, address};
+        if (validate(array)) {
+            long phone1 = Long.parseLong(phone);
+            User user = new User(name, generateUserId(), address, phone1); // generateUserId is a method that returns a unique user ID
+            Storage.users.add(user);
+            System.out.println("User created successfully. ID: " + user.getId());
+        }
+        else {
+            System.err.println("Missing required parameters. Please provide -n, -p, -a");
+        }
+    }
+    @Override
+    public boolean validate(String[] args) {
+        String name = args[0];
+        String phone = args[1];
+        String address = args[2];
+
+        if (name == null || phone == null || address == null) {
+            System.err.println("Missing required parameters. Please provide -n, -p, -a");
+            return false;
+        }
+        if (userExists(name, phone)) {
+            System.err.println("User with the same name or phone number already exists. Cannot create duplicate users.");
+            return false;
+        }
+        return true;
+    }
+    private boolean userExists(String name, String phone) {
+        long phone1 = Long.parseLong(phone);
+        for (User existingUser : Storage.users) {
+            if (existingUser.getName().equals(name) || existingUser.getPhone() == phone1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String generateUserId() {
+        // This is a simple method to generate a unique user ID. You might want to use a better approach in a real application.
+        return "U" + System.currentTimeMillis(); // System.currentTimeMillis() returns the current time in milliseconds
+    }
+
+}
