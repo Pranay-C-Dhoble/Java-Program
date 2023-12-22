e2e768c4f4676a1780e54a14a2f074aaa34071a3
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-22 23:23:05 +0530

    Create Result.java

---
+++ src/JavaProjects/NewJavaProject/Model/Result.java
@@ -0,0 +1,12 @@
+package JavaProjects.NewJavaProject.Model;
+
+public class Result {
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
