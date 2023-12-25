95484f9b2cacb0d816dfbada7693a570bc05e1be
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-25 13:45:47 +0530

    Create ReturnIndicesOfTheTwoNumbersSuchThatTheyAddUpToTarget.java

---
+++ src/array_programs_using_methods/ReturnIndicesOfTheTwoNumbersSuchThatTheyAddUpToTarget.java
@@ -0,0 +1,20 @@
+package array_programs_using_methods;
+
+public class ReturnIndicesOfTheTwoNumbersSuchThatTheyAddUpToTarget {
+    public static void main(String[] args) {
+        int [] arr = {3,2,4};
+        int target = 6;
+        int [] ans = twoSum(arr,target);
+        System.out.println("The indices of the two numbers such that they add up to target are : "+java.util.Arrays.toString(ans));
+    }
+    public static int[] twoSum(int []num, int target){
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++){
+                if(target==num[i]+num[j]){
+                    return new int[] {i,j};// returning indices of the two numbers such that they add up to target
+                }
+            }
+        }
+        return null;
+    }
+}
